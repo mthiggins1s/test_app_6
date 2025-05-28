b4f1f38a24dbc239dd5fdf5aff063d8aff930b2c
Matthew Higgins <mthiggins1s@semo.edu>
2025-05-28 15:15:47 -0500

    Add PagesController and home view; define root route

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,6 @@
 class ApplicationController < ActionController::Base
-  # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
-  allow_browser versions: :modern
+  # We will define a method in here, where we want to send the request to.
+    # Within the routes.rb file, to specify a METHOD, is that you put a # sign AFTER the controller name, and then specify the name of the method
+    def home
+    end
 end
